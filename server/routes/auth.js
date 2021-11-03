c1633d658f3177efe2dbed794d07015ebe1c5567
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2021-11-04 05:25:16 +0530

    Netlify, Heroku cookie fix

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,14 +9,14 @@ const SALT_ROUNDS = 10
 const ACCESS_COOKIE_OPTIONS = {
     httpOnly: true,
     secure: false,
-    sameSite: 'none',
+    domain: process.env.SERVER_ENDPOINT,
     path: '/',
     maxAge: 2 * 60 * 1000
 }
 const REFRESH_COOKIE_OPTIONS = {
     httpOnly: true,
     secure: false,
-    sameSite: 'none',
+    domain: process.env.SERVER_ENDPOINT,
     path: '/',
     maxAge: 10 * 60 * 1000
 }
