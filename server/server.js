db6b174ce55ad402fe3638b59aa354f1e048de7f
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-12-02 12:40:45 +0530

    Revert "Abstract CORS"
    This reverts commit 7610409605e04486d46bf1c7433df29d5770cc16.

--- server/server.js
+++ server/server.js
@@ -7,16 +7,11 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 const authRoutes = require('./routes/auth')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
-const cors = require('cors')
 
 mongoose.connect(process.env.MONGO_ATLAS_URI)
 const db = mongoose.connection
-db.on('error', (error) => {
-	console.error(error)
-})
-db.once('open', () => {
-	console.log('Connected to database')
-})
+db.on('error', error => { console.error(error) })
+db.once('open', () => { console.log('Connected to database') })
 
 const PORT = process.env.PORT || 5000
 
@@ -24,93 +19,97 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept')
+  next()
+})
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_ENDPOINT }))
 
 app.use('/auth', authRoutes)
 
-io.on('connection', (socket) => {
-	socket.on('waiting', () => {
-		socket.join('waitingRoom')
+io.on('connection', socket => {
+    
+    socket.on('waiting', () => {
+        socket.join('waitingRoom')
 
-		const waitingClients = io.sockets.adapter.rooms.get('waitingRoom')
+        const waitingClients = io.sockets.adapter.rooms.get('waitingRoom')
 
-		io.to('waitingRoom').emit('waitingRoomData', {
-			waiting: [...waitingClients],
-		})
-	})
+        io.to('waitingRoom').emit('waitingRoomData', {waiting: [... waitingClients]})
+    })
 
-	socket.on('waitingDisconnection', (id) => {
-		if (id) io.sockets.sockets.get(id).leave('waitingRoom')
-		else socket.leave('waitingRoom')
+    socket.on('waitingDisconnection', (id) => {
 
-		const waitingClients = io.sockets.adapter.rooms.get('waitingRoom')
+        if (id) io.sockets.sockets.get(id).leave('waitingRoom')
+        else socket.leave('waitingRoom')
 
-		if (waitingClients) {
-			io.to('waitingRoom').emit('waitingRoomData', {
-				waiting: [...waitingClients],
-			})
-			socket.emit('waitingRoomData', { waiting: [...waitingClients] })
-		} else {
-			io.to('waitingRoom').emit('waitingRoomData', { waiting: [] })
-			socket.emit('waitingRoomData', { waiting: [] })
-		}
-	})
+        const waitingClients = io.sockets.adapter.rooms.get('waitingRoom')
 
-	socket.on('randomCode', ({ id1, id2, code }) => {
-		id1 && io.sockets.sockets.get(id1).emit('randomCode', { code: code })
-		id2 && io.sockets.sockets.get(id2).emit('randomCode', { code: code })
-	})
+        if (waitingClients) {
+            io.to('waitingRoom').emit('waitingRoomData', {waiting: [... waitingClients]})
+            socket.emit('waitingRoomData', {waiting: [... waitingClients]})    
+        }
 
-	socket.on('join', (payload, callback) => {
-		let numberOfUsersInRoom = getUsersInRoom(payload.room).length
+        else {
+            io.to('waitingRoom').emit('waitingRoomData', {waiting: []})
+            socket.emit('waitingRoomData', {waiting: []})
+        }
+    })
 
-		const { error, newUser } = addUser({
-			id: socket.id,
-			name: numberOfUsersInRoom === 0 ? 'Player 1' : 'Player 2',
-			room: payload.room,
-		})
+    socket.on('randomCode', ({id1, id2, code}) => {
+        id1 && io.sockets.sockets.get(id1).emit('randomCode', {code: code})
+        id2 && io.sockets.sockets.get(id2).emit('randomCode', {code: code})
+    })
 
-		if (error) return callback(error)
+    socket.on('join', (payload, callback) => {
+        let numberOfUsersInRoom = getUsersInRoom(payload.room).length
 
-		socket.join(newUser.room)
+        const { error, newUser } = addUser({
+            id: socket.id,
+            name: numberOfUsersInRoom===0 ? 'Player 1' : 'Player 2',
+            room: payload.room
+        })
 
-		io.to(newUser.room).emit('roomData', {
-			room: newUser.room,
-			users: getUsersInRoom(newUser.room),
-		})
-		socket.emit('currentUserData', { name: newUser.name })
-		callback()
-	})
+        if(error)
+            return callback(error)
 
-	socket.on('initGameState', (gameState) => {
-		const user = getUser(socket.id)
-		if (user) io.to(user.room).emit('initGameState', gameState)
-	})
+        socket.join(newUser.room)
 
-	socket.on('updateGameState', (gameState) => {
-		const user = getUser(socket.id)
+        io.to(newUser.room).emit('roomData', {room: newUser.room, users: getUsersInRoom(newUser.room)})
+        socket.emit('currentUserData', {name: newUser.name})
+        callback()
+    })
 
-		if (user) io.to(user.room).emit('updateGameState', gameState)
-	})
+    socket.on('initGameState', gameState => {
+        const user = getUser(socket.id)
+        if(user)
+            io.to(user.room).emit('initGameState', gameState)
+    })
 
-	socket.on('sendMessage', (payload, callback) => {
-		const user = getUser(socket.id)
-		io.to(user.room).emit('message', { user: user.name, text: payload.message })
-		callback()
-	})
+    socket.on('updateGameState', gameState => {
+        const user = getUser(socket.id)
+        
+        if(user)
+            io.to(user.room).emit('updateGameState', gameState)
+    })
 
-	socket.on('disconnection', () => {
-		const user = removeUser(socket.id)
-		if (user)
-			io.to(user.room).emit('roomData', {
-				room: user.room,
-				users: getUsersInRoom(user.room),
-			})
-	})
+    socket.on('sendMessage', (payload, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('message', {user: user.name, text: payload.message})
+        callback()
+    })
+
+    socket.on('disconnection', () => {
+        const user = removeUser(socket.id)
+        if(user)
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+    })
 })
 
+
 server.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
