ebf1e20c68131c1b5b208809ec35ad4b83553dce
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2022-12-03 06:31:07 +0530

    Update CORS middleware

--- server/server.js
+++ server/server.js
@@ -24,26 +24,8 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.use(function (req, res, next) {
-	res.header('Access-Control-Allow-Credentials', true)
-	res.header('Access-Control-Allow-Origin', req.headers.referer)
-	res.header(
-		'Access-Control-Allow-Methods',
-		'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
-	)
-	res.header(
-		'Access-Control-Allow-Headers',
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-	)
-	//intercepts OPTIONS method
-	if ('OPTIONS' === req.method) {
-		//respond with 200
-		res.send(200)
-	} else {
-		//move on
-		next()
-	}
-})
+app.use(cors())
+app.options('*', cors())
 app.use(express.json())
 app.use(cookieParser())
 
