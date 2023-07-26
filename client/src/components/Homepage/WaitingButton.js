1adc01dd3262d88b31c73d4676a7a0e906125bfb
Raghuboi <82239949+Raghuboi@users.noreply.github.com>
2023-07-26 06:19:03 -0500

    Add info message for server cold start

--- client/src/components/Homepage/WaitingButton.js
+++ client/src/components/Homepage/WaitingButton.js
@@ -1,38 +1,58 @@
 import React from 'react'
 import {
-    Popover,
-    Portal,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverArrow,
-    PopoverCloseButton,
-    PopoverBody,
-    Button,
-    Heading,
-    Spinner,
-    HStack
+	Popover,
+	Portal,
+	PopoverTrigger,
+	PopoverContent,
+	PopoverArrow,
+	PopoverCloseButton,
+	PopoverBody,
+	Button,
+	Heading,
+	Text,
+	Spinner,
+	HStack,
+	VStack,
 } from '@chakra-ui/react'
 
-export default function WaitingButton({ w, size, onClose, onTrigger, queueLength }) {
-    return (
-        <>
-        <Popover flip={true} returnFocusOnClose={true} onClose={onClose} >
-            <PopoverTrigger>
-            <Button w={w} size={size} onClick={onTrigger}>Matchmaking</Button>
-            </PopoverTrigger>
-        <Portal>
-            <PopoverContent w="fit-content" p="1rem 1.5rem" className="noselect" >
-                <PopoverArrow/>
-                <PopoverCloseButton/>
-                <PopoverBody>
-                    <HStack s="1rem">
-                        <Spinner/>
-                        <Heading size="md">{"Waiting, "+queueLength+" in Queue."}</Heading>
-                    </HStack>
-                </PopoverBody>
-            </PopoverContent>
-        </Portal>
-        </Popover>
-        </>
-    )
+export default function WaitingButton({
+	w,
+	size,
+	onClose,
+	onTrigger,
+	queueLength,
+}) {
+	return (
+		<>
+			<Popover flip={true} returnFocusOnClose={true} onClose={onClose}>
+				<PopoverTrigger>
+					<Button w={w} size={size} onClick={onTrigger}>
+						Matchmaking
+					</Button>
+				</PopoverTrigger>
+				<Portal>
+					<PopoverContent w='fit-content' p='1rem 1.5rem' className='noselect'>
+						<PopoverArrow />
+						<PopoverCloseButton />
+						<PopoverBody>
+							<VStack>
+								<HStack s='1rem'>
+									<Spinner />
+									<Heading size='md'>
+										{'Waiting, ' + queueLength + ' in Queue.'}
+									</Heading>
+								</HStack>
+								{queueLength === 0 && (
+									<Text size='sm'>
+										Please be patient. The server can take upto 1 min to cold
+										start.{' '}
+									</Text>
+								)}
+							</VStack>
+						</PopoverBody>
+					</PopoverContent>
+				</Portal>
+			</Popover>
+		</>
+	)
 }
